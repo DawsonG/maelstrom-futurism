6336fc8450964d8c949e7947cf269cef63651c4d
Dawson Goodell <dawsong@osmstudios.com>
2020-09-06 00:57:10 -0700

    globalObject this hack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
     library: "maelstrom-futurism",
-    libraryTarget: "umd"
+    libraryTarget: "umd",
+    globalObject: "this"
   },
   externals: [nodeExternals()],
 
