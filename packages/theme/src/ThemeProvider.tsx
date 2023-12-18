89532bcdd86c78bcc6ad52d06a54cd1cd5ab2935
Dawson Goodell <dawsong@osmstudios.com>
2023-12-17 17:07:55 -0800

    feat(theme): reconfigure theme and themeprovider so projects can override

---
+++ packages/theme/src/ThemeProvider.tsx
@@ -0,0 +1,73 @@
+import React, { ReactNode, createContext, useContext } from 'react';
+import { Global, css } from '@emotion/react';
+import poppinsLight from './fonts/Poppins-Light.ttf';
+import poppinsMedium from './fonts/Poppins-Medium.ttf';
+import silkscreenMedium from './fonts/Silkscreen-Regular.ttf';
+import silkscreenBold from './fonts/Silkscreen-Bold.ttf';
+import Theme from './theme';
+
+const defaultValue = new Theme("User");
+const ThemeContext = createContext(defaultValue);
+export const useTheme = () => useContext(ThemeContext);
+
+interface ThemeProviderProps {
+    theme?: Theme;
+    children: ReactNode;
+}
+
+export const ThemeProvider = ({ theme, children }: ThemeProviderProps): JSX.Element => {
+    const themeValue = theme || new Theme("User");
+
+    const globalStyles = css`
+        html {
+            box-sizing: border-box;
+        }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
+
+        @font-face {
+            font-family: "Poppins";
+            font-weight: 400;
+            src: url(${poppinsLight}) format('truetype');
+        }
+        
+        @font-face {
+            font-family: "Poppins";
+            font-weight: 500;
+            src: url(${poppinsMedium}) format('truetype');
+        }
+        
+        @font-face {
+            font-family: "Silkscreen";
+            font-weight: 400;
+            src: url(${silkscreenMedium}) format('truetype');
+        }
+        
+        @font-face {
+            font-family: "Silkscreen";
+            font-weight: 600;
+            src: url(${silkscreenBold}) format('truetype');
+        }
+
+        html, body {
+            min-height: 100vh;
+        }
+        body {
+            color: ${themeValue.color("text")};
+            background-color: ${themeValue.color("background")};
+            font-family: Poppins;
+        }
+
+        h1, h2, h3, h4 {
+            margin: ${themeValue.space[1]}px 0;
+        }
+    `;
+
+    return (
+        <ThemeContext.Provider value={themeValue}>
+            <Global styles={globalStyles} />
+            {children}
+        </ThemeContext.Provider>
+    );
+};
---
+++ packages/theme/src/theme.ts
@@ -0,0 +1,87 @@
+export type ColorMode = "Light" | "Dark" | "User";
+
+class Theme {
+  private colorMode: ColorMode;
+
+  sizes = {
+    base: "16px",
+    sm: "0.707em",
+    normal: "1em",
+    md: "1.414em",
+    lg: "2em",
+    xl: "2.827em",
+    xxl: "4em",
+  };
+
+  breakpoints = {
+    xs: "0px",
+    sm: "576px",
+    md: "992px",
+    lg: "1200px",
+  };
+
+  space = [0, 4, 8, 16, 24, 32, 40, 48, 64];
+
+  colors = {
+    trueWhite: "#FFFFFF",
+    trueBlack: "#000000",
+    font: "#16191F",
+    border: "#B4B4B4",
+    muted: "#707070",
+    active: "#2196F3",
+
+    primary: "#88c0d0",
+  };
+
+  colorsDark = {
+    background: "#2e3440",
+    backgroundPanel: "#3b4252",
+    text: "#eceff4",
+    interactable: "#3b4252",
+
+    // temporary
+    border: "#d8dee9",
+    muted: "#707070",
+    active: "#2196F3",
+
+    primary: "#88c0d0",
+  };
+
+  colorsLight = {
+    background: "",
+  };
+
+  heights = {
+    "very low": "-1000",
+    low: "-100",
+    sunk: "-10",
+    normal: "0",
+    raised: "10",
+    high: "100",
+    "very high": "1000",
+  };
+
+  borderRadius = (): string => {
+    return `${this.space[3]}px`;
+  }
+
+  size(name: string): string {
+    return this.sizes[name];
+  }
+
+  color = (name: string): string => {
+    if (this.colorMode === "Light") {
+      return this.colorsLight[name];
+    } else /* if (this.colorMode === "Dark") */{
+      return this.colorsDark[name];
+    }
+  };
+  bp = (name: string): string => this.breakpoints[name];
+  height = (name: string): string => this.heights[name];
+
+  constructor(colorMode: ColorMode) {
+    this.colorMode = colorMode;
+  }
+}
+
+export default Theme;
