c1c59980f4fe58539e652f4ce73a2c816a1dcf7b
Dawson Goodell <dawsong@osmstudios.com>
2025-01-09 23:05:29 -0800

    feat: add maelstrom-futurism superset package for easy imports

---
+++ packages/maelstrom-futurism/index.ts
@@ -0,0 +1,5 @@
+export * from '@maelstrom-futurism/button';
+export * from '@maelstrom-futurism/core';
+export * from '@maelstrom-futurism/input';
+export * from '@maelstrom-futurism/layout';
+export * from '@maelstrom-futurism/paper';
