12613322025df2a2aac51af9a8fbac5414646732
Dawson Goodell <dawsong@osmstudios.com>
2025-01-12 00:01:55 -0800

    feat: create a script that quickly sets up a new package in the packages directory

---
+++ bin/createPackage.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+
+const [name, description] = process.argv.slice(2);
+
+const packageJson = `{
+  "name": "${name}",
+  "version": "0.7.0",
+  "description": "${description}",
+  "main": "dist/index.js",
+  "module": "dist/index.js",
+  "types": "dist/index.d.ts",
+  "typings": "dist/index.d.ts",
+  "exports": {
+    ".": {
+      "require": "./dist/index.js",
+      "default": "./dist/index.js"
+    }
+  },
+  "files": [
+    "dist"
+  ],
+  "scripts": {
+    "build": "tsc",
+    "test": "vitest run --coverage",
+    "test:dev": "vitest"
+  },
+  "author": "Dawson Goodell <dawsong@osmstudios.com>",
+  "homepage": "https://osmstudios.com",
+  "license": "SEE LICENSE IN LICENSE.txt",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/DawsonG/maelstrom-futurism.git",
+    "directory": "packages/{name}"
+  },
+  "dependencies": {
+    "@maelstrom-futurism/core": "../core"
+  },
+  "peerDependences": {
+    "react": ">= 18.0.0",
+    "react-dom": ">= 18.0.0"
+  }
+}`;
+
+const tsconfigJson = `{
+    "extends": "../../tsconfig.json",
+    "include": ["src"],
+    "compilerOptions": {
+        "outDir": "dist",
+        "jsxImportSource": "@emotion/react",
+        "jsx": "react-jsx",
+        "sourceMap": true,
+        "declaration": true,
+        "declarationDir": "dist"
+    }
+}`;
+
+const dir = path.join('packages', name)
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+fs.writeFile(path.join(dir, 'package.json'), packageJson, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+});
+
+fs.writeFile(path.join(dir, 'tsconfig.json'), tsconfigJson, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+});
