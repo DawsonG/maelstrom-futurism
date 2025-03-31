328964b38c859472bf51897a158889041e99f8fd
Dawson Goodell <dawsong@osmstudios.com>
2025-03-31 03:07:05 +0000

    feat(CLI): improve CLI tools to speed up development

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import { Command  } from "commander";
+import createPackage from "./createPackage.js";
+
+const program = new Command();
+
+program
+    .name("mf")
+    .description("CLI for simplifying actions related to Maelstrom-Futurism")
+    .version("1.0.0");
+
+program.command("create-package")
+    .description("creates a new component package in the packages folder")
+    .argument("<string>", "name of the new package")
+    .option("-d, --description <string>", "description of the package to be created. Used in the package.json", "")
+    .option("--pv, --packageVersion <string>", "sets the version in the package.json", "0.7.0")
+    .action(createPackage);
+
+program.parse();
--- bin/createPackage.js
+++ bin/createPackage.js
@@ -1,46 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
-const [name, description] = process.argv.slice(2);
-
-const packageJson = `{
-  "name": "${name}",
-  "version": "0.7.0",
-  "description": "${description}",
-  "main": "dist/index.js",
-  "module": "dist/index.js",
-  "types": "dist/index.d.ts",
-  "typings": "dist/index.d.ts",
-  "exports": {
-    ".": {
-      "require": "./dist/index.js",
-      "default": "./dist/index.js"
-    }
-  },
-  "files": [
-    "dist"
-  ],
-  "scripts": {
-    "build": "tsc",
-    "test": "vitest run --coverage",
-    "test:dev": "vitest"
-  },
-  "author": "Dawson Goodell <dawsong@osmstudios.com>",
-  "homepage": "https://osmstudios.com",
-  "license": "SEE LICENSE IN LICENSE.txt",
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/DawsonG/maelstrom-futurism.git",
-    "directory": "packages/{name}"
-  },
-  "dependencies": {
-    "@maelstrom-futurism/core": "../core"
-  },
-  "peerDependences": {
-    "react": ">= 18.0.0",
-    "react-dom": ">= 18.0.0"
-  }
-}`;
+import fs from 'fs';
+import path from 'path';
+import { printHeader } from './utils.js';
 
 const tsconfigJson = `{
     "extends": "../../tsconfig.json",
@@ -55,17 +15,83 @@ const tsconfigJson = `{
     }
 }`;
 
-const dir = path.join('packages', name);
-const src = path.join(dir, 'src');
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-    fs.mkdirSync(src);
+const createPackage = (name, options) => {
+  const { description, packageVersion } = options;
+  const dir = path.join('packages', name);
+  const src = path.join(dir, 'src');
+  
+  const packageJson = `{
+    "name": "${name}",
+    "version": "${packageVersion}",
+    "description": "${description}",
+    "main": "dist/index.js",
+    "module": "dist/index.js",
+    "types": "dist/index.d.ts",
+    "typings": "dist/index.d.ts",
+    "exports": {
+      ".": {
+        "require": "./dist/index.js",
+        "default": "./dist/index.js"
+      }
+    },
+    "files": [
+      "dist"
+    ],
+    "scripts": {
+      "build": "tsc",
+      "test": "vitest run --coverage",
+      "test:dev": "vitest"
+    },
+    "author": "Dawson Goodell <dawsong@osmstudios.com>",
+    "homepage": "https://osmstudios.com",
+    "license": "SEE LICENSE IN LICENSE.txt",
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/DawsonG/maelstrom-futurism.git",
+      "directory": "packages/{name}"
+    },
+    "dependencies": {
+      "@maelstrom-futurism/core": "../core"
+    },
+    "peerDependences": {
+      "react": ">= 18.0.0",
+      "react-dom": ">= 18.0.0"
+    }
+  }`;
+  
+  printHeader();
+  
+  console.log(`Creating a new package with the following properties:
+{
+  "name": "${name}",
+  "version": "${packageVersion}",
+  "description": "${description}",
+  ...
 }
+  `);
 
-fs.writeFile(path.join(dir, 'package.json'), packageJson, (err) => {
-    if (err) throw err;
-});
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+      fs.mkdirSync(src);
+      console.log(`New package directory created ${dir}.
+New src directory created ${src}.`);
+  } else {
+      console.error(`package with name ${name} already exists! Exiting...`);
+      return false;
+  }
 
-fs.writeFile(path.join(dir, 'tsconfig.json'), tsconfigJson, (err) => {
-    if (err) throw err;
-});
+  try {
+    fs.writeFileSync(path.join(dir, 'package.json'), packageJson);
+    console.log("package.json created");
+
+    fs.writeFileSync(path.join(dir, 'tsconfig.json'), tsconfigJson);
+    console.log("tsconfig.json created");
+  } catch (err) {
+    console.error("Error creating files");
+    throw err;
+  }
+
+  console.log("\n\n\nDone! Have fun with your new package.");
+};
+
+export default createPackage;
