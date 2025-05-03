e3d96a2b09353dc46d6c5db5b74cc4ff8e668117
Dawson Goodell <dawsong@osmstudios.com>
2025-05-03 17:25:39 +0000

    fix: sidebar import of <Button/>

--- packages/sidebar/src/Heading.tsx
+++ packages/sidebar/src/Heading.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import Button from "@maelstrom-futurism/button";
+import { Button } from "@maelstrom-futurism/button";
 import { useTheme } from "@maelstrom-futurism/core";
 import { Icon } from "@maelstrom-futurism/icons";
 
