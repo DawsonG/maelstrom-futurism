e995e0be474fcff3489933c76332ebaa3486f871
Dawson Goodell <dawsong@osmstudios.com>
2025-05-03 17:43:45 +0000

    chore: cleanup more src/ folder items that are now in packages or obsolete

--- src/navbar/children/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Group } from "./Group";
-export { default as Link } from "./Link";
-export { default as Brand } from "./Brand";
-// export { default as Dropdown } from './Dropdown';
