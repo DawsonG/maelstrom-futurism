09d6644703bd578a737c3d3e36c30dcebc0a867c
Dawson Goodell <dawsong@osmstudios.com>
2025-05-15 05:51:14 +0000

    fix: Caret icons should have generic currentColor fills

--- website/src/pages/Icons.tsx
+++ website/src/pages/Icons.tsx
@@ -11,6 +11,10 @@ const Icons = () => (
         <Icon icon="AngleRight" />
         <Icon icon="AngleDown" />
         <Icon icon="AngleLeft" />
+        <Icon icon="CaretUp" />
+        <Icon icon="CaretRight" />
+        <Icon icon="CaretDown" />
+        <Icon icon="CaretLeft" />
         <Icon icon="Copy" />
         <Icon icon="MenuBurger" />
 
