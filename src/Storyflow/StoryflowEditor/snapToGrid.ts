b58807f3d1b7958b15189536d400ef467c7a08de
Dawson Goodell <dawsong@osmstudios.com>
2025-05-23 06:00:00 +0000

    chore: remove deprecated Storyflow component

--- src/Storyflow/StoryflowEditor/snapToGrid.ts
+++
@@ -1,5 +0,0 @@
-export function snapToGrid(physicalXY: Array<number>): [number, number] {
-  const snappedX = Math.round(physicalXY[0] / 32) * 32;
-  const snappedY = Math.round(physicalXY[1] / 32) * 32;
-  return [snappedX, snappedY];
-}
