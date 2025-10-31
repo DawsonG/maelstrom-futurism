f6f40005c9879db258576a16a95be483105d3a0b
Dawson Goodell <dawsong@osmstudios.com>
2025-10-31 05:00:00 +0000

    chore: rearrange tsconfig and cleanup dependencies

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useRef } from "react";
+import React, { useRef } from "react";
 
 import { buttonStyle } from "./Button.styles";
 import { useTheme, debounce, Theme } from "@maelstrom-futurism/core";
