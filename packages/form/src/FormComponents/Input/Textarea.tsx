234ba2f5c408a90617bf9826adce19a456c46679
Dawson Goodell <dawsong@osmstudios.com>
2026-03-12 19:30:46 +0000

    fix: Textarea needs to display the passed value

--- packages/form/src/FormComponents/Input/Textarea.tsx
+++ packages/form/src/FormComponents/Input/Textarea.tsx
@@ -32,7 +32,7 @@ const TextArea = ({ name, label, value, ...rest }: TextAreaProps) => {
     return (
         <div css={fcContainer}>
             {label && <label htmlFor={name}>{label}</label>}
-            <textarea css={textAreaStyles} {...rest}></textarea>
+            <textarea css={textAreaStyles} {...rest}>{value}</textarea>
         </div>
     );
 };
