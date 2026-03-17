94da377737304cda77b75be633a8be27af3f8162
Dawson Goodell <dawsong@osmstudios.com>
2026-03-17 21:29:15 +0000

    feat: add shopping related icons

--- packages/icons/lib/icons.ts
+++ packages/icons/lib/icons.ts
@@ -9,10 +9,12 @@ import CaretRight from "../assets/caret-right.svg";
 import CaretUp from "../assets/caret-up.svg";
 import Copy from "../assets/copy.svg";
 import Home from "../assets/home.svg";
+import Heart from "../assets/heart.svg";
 import MenuBurger from "../assets/menu-burger.svg";
 import Moon from "../assets/moon.svg";
 import Upload from "../assets/upload.svg";
 import Sun from "../assets/sun.svg";
+import ShoppingCart from "../assets/shopping-cart.svg";
 
 export const icons = {
     Add,
@@ -26,8 +28,10 @@ export const icons = {
     CaretUp,
     Copy,
     Home,
+    Heart,
     MenuBurger,
     Moon,
     Upload,
-    Sun
+    Sun,
+    ShoppingCart,
 };
