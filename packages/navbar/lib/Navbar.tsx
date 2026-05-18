7e5fa56138d3b8c860c306bb1535d7709c44c6a4
Dawson Goodell <dawsong@osmstudios.com>
2026-05-18 23:01:26 +0000

    chore: lint @maelstrom-futurism/navbar

--- packages/navbar/lib/Navbar.tsx
+++ packages/navbar/lib/Navbar.tsx
@@ -4,19 +4,19 @@ import { Container } from '@maelstrom-futurism/layout';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
-    brand?: ReactElement;
-    rightSide?: ReactElement;
+  brand?: ReactElement;
+  rightSide?: ReactElement;
 }
 
 const Navbar = ({ brand, rightSide }: NavbarProps): ReactNode => {
-    return (
-        <div className={styles.navbarRow}>
-            <Container className={styles.navbarWrapper}>
-                {brand}
-                {rightSide}
-            </Container>
-        </div>
-    );
-}
+  return (
+    <div className={styles.navbarRow}>
+      <Container className={styles.navbarWrapper}>
+        {brand}
+        {rightSide}
+      </Container>
+    </div>
+  );
+};
 
 export default Navbar;
