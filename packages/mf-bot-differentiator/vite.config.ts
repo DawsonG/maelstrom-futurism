ffcfc1a79347d3bb3667e9c364f338fe4720ddaf
Dawson Goodell <dawsong@osmstudios.com>
2026-07-09 20:11:51 +0000

    fix: move mf/mf-bot-differentiator to use vite build

---
+++ packages/mf-bot-differentiator/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    dts({ include: ['src'], exclude: ['src/**/*.test.*'] }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react/jsx-runtime',
+        'react-dom',
+        '@maelstrom-futurism/core',
+        '@maelstrom-futurism/paper',
+      ],
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+        },
+      },
+    },
+  },
+});
